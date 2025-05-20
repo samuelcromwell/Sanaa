b7b000ef857ad9ed977943ea10489d2a6aad5129
samuelcromwell <cromwellsamuel3@gmail.com>
2025-05-20 22:21:19 +0300

    Bg images on slider changed to CDN and swiper works

--- components/sections/home1/Banner.js
+++ components/sections/home1/Banner.js
@@ -1,125 +1,142 @@
-
 'use client'
 import Link from "next/link"
 import { Autoplay, Navigation, Pagination } from "swiper/modules"
 import { Swiper, SwiperSlide } from "swiper/react"
 import { useState } from 'react'
 
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
 const swiperOptions = {
     modules: [Autoplay, Pagination, Navigation],
     slidesPerView: 1,
     spaceBetween: 0,
-    
     loop: true,
-
-    // Navigation
-    navigation: {
-        nextEl: '.h1n',
-        prevEl: '.h1p',
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
     },
-
-    // Pagination
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
     },
-
-
-
 }
 
-
 export default function Banner() {
-
     const [isOpen, setOpen] = useState(false)
+
     return (
         <>
+            {/* Main Slider Start */}
+            <section className="main-slider">
+                <Swiper {...swiperOptions} className="main-slider__carousel">
+                    <SwiperSlide>
+                        <div className="item">
+                            <div
+                                className="main-slider__bg"
+                                style={{
+                                    backgroundImage: `url('https://res.cloudinary.com/dhgcuzl2x/image/upload/v1747768595/slider01_ostlcu.jpg')`,
+                                }}
+                            />
+                            <div className="main-slider__shape-1"></div>
+                            <div className="main-slider__shape-2"></div>
+                            <div className="main-slider__shape-3"></div>
+                            <div className="main-slider__img">
+                                {/* <img src="assets/images/resources/main-slider-3.jpeg" alt="" loading="lazy" /> */}
+                            </div>
+                            <div className="container">
+                                <div className="main-slider__content">
+                                    <div className="main-slider__sub-title-box">
+                                        <p className="main-slider__sub-title">Electrical Company</p>
+                                        <div className="main-slider__sub-title-shape"></div>
+                                    </div>
+                                    <h2 className="main-slider__title">Power Up <br /> Your World</h2>
+                                    <p className="main-slider__text">
+                                        We have been illuminating homes and businesses for over three years, providing <br />
+                                        top-tier electrical solutions and unmatched customer service.
+                                    </p>
+                                    <div className="main-slider__btn-box">
+                                        <Link href="about" className="main-slider__btn thm-btn">Learn More</Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </SwiperSlide>
 
-        {/* Main Sllider Start */}
-        <section className="main-slider">
-            <Swiper {...swiperOptions} className="main-slider__carousel owl-carousel owl-theme">
-                <SwiperSlide>
-                <div className="item">
-                    <div className="main-slider__bg"
-                        style={{ backgroundImage: 'url(assets/images/backgrounds/slider01.jpg)' }} >
-                    </div>{/* /.slider-one__bg */}
-                    <div className="main-slider__shape-1"></div>
-                    <div className="main-slider__shape-2"></div>
-                    <div className="main-slider__shape-3"></div>
-                    <div className="main-slider__img">
-                        {/* <img src="assets/images/resources/main-slider-3.jpeg" alt=""/> */}
-                    </div>
-                    <div className="container">
-                        <div className="main-slider__content">
-                            <div className="main-slider__sub-title-box">
-                                <p className="main-slider__sub-title">Electrical Company</p>
-                                <div className="main-slider__sub-title-shape"></div>
+                    <SwiperSlide>
+                        <div className="item">
+                            <div
+                                className="main-slider__bg"
+                                style={{
+                                    backgroundImage: `url('https://res.cloudinary.com/dhgcuzl2x/image/upload/v1747768509/slider2_nzfzpt.png')`,
+                                }}
+                            />
+                            <div className="main-slider__shape-1"></div>
+                            <div className="main-slider__shape-2"></div>
+                            <div className="main-slider__shape-3"></div>
+                            <div className="main-slider__img">
+                                {/* <img src="assets/images/resources/main-slider-img-1-2.png" alt="" loading="lazy" /> */}
                             </div>
-                            <h2 className="main-slider__title">Power Up <br/> Your World</h2>
-                            <p className="main-slider__text">We have been illuminating homes and businesses for over three years, providing <br/> top-tier electrical solutions and unmatched customer service.</p>
-                            <div className="main-slider__btn-box">
-                                <Link href="about" className="main-slider__btn thm-btn">Learn More</Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                <div className="item">
-                    <div className="main-slider__bg"
-                        style={{ backgroundImage: 'url(assets/images/backgrounds/slider2.png)' }} >
-                    </div>{/* /.slider-one__bg */}
-                    <div className="main-slider__shape-1"></div>
-                    <div className="main-slider__shape-2"></div>
-                    <div className="main-slider__shape-3"></div>
-                    <div className="main-slider__img">
-                        {/* <img src="assets/images/resources/main-slider-img-1-2.png" alt=""/> */}
-                    </div>
-                    <div className="container">
-                        <div className="main-slider__content">
-                            <div className="main-slider__sub-title-box">
-                                <p className="main-slider__sub-title">Electrical Company</p>
-                                <div className="main-slider__sub-title-shape"></div>
-                            </div>
-                            <h2 className="main-slider__title">Power Up <br/> Your World</h2>
-                            <p className="main-slider__text">We have been illuminating homes and businesses for over three years, providing <br/> top-tier electrical solutions and unmatched customer service.</p>
-                            <div className="main-slider__btn-box">
-                                <Link href="about" className="main-slider__btn thm-btn">Learn More</Link>
+                            <div className="container">
+                                <div className="main-slider__content">
+                                    <div className="main-slider__sub-title-box">
+                                        <p className="main-slider__sub-title">Electrical Company</p>
+                                        <div className="main-slider__sub-title-shape"></div>
+                                    </div>
+                                    <h2 className="main-slider__title">Power Up <br /> Your World</h2>
+                                    <p className="main-slider__text">
+                                        We have been illuminating homes and businesses for over three years, providing <br />
+                                        top-tier electrical solutions and unmatched customer service.
+                                    </p>
+                                    <div className="main-slider__btn-box">
+                                        <Link href="about" className="main-slider__btn thm-btn">Learn More</Link>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                <div className="item">
-                    <div className="main-slider__bg"
-                        style={{ backgroundImage: 'url(assets/images/backgrounds/slider3.jpeg)' }} >
-                    </div>{/* /.slider-one__bg */}
-                    <div className="main-slider__shape-1"></div>
-                    <div className="main-slider__shape-2"></div>
-                    <div className="main-slider__shape-3"></div>
-                    <div className="main-slider__img">
-                        {/* <img src="assets/images/resources/main-slider-img-1-3.png" alt=""/> */}
-                        {/* main-slider-3 is the image to put */}
-                    </div>
-                    <div className="container">
-                        <div className="main-slider__content">
-                            <div className="main-slider__sub-title-box">
-                                <p className="main-slider__sub-title">Electrical Company</p>
-                                <div className="main-slider__sub-title-shape"></div>
+                    </SwiperSlide>
+
+                    <SwiperSlide>
+                        <div className="item">
+                            <div
+                                className="main-slider__bg"
+                                style={{
+                                    backgroundImage: `url('https://res.cloudinary.com/dhgcuzl2x/image/upload/v1747768514/slider3_jdzlvr.jpg')`,
+                                }}
+                            />
+                            <div className="main-slider__shape-1"></div>
+                            <div className="main-slider__shape-2"></div>
+                            <div className="main-slider__shape-3"></div>
+                            <div className="main-slider__img">
+                                {/* <img src="assets/images/resources/main-slider-img-1-3.png" alt="" loading="lazy" /> */}
+                                {/* main-slider-3 is the image to put */}
                             </div>
-                            <h2 className="main-slider__title">Power Up <br/> Your World</h2>
-                            <p className="main-slider__text">We have been illuminating homes and businesses for over three years, providing <br/> top-tier electrical solutions and unmatched customer service.</p>
-                            <div className="main-slider__btn-box">
-                                <Link href="about" className="main-slider__btn thm-btn">Learn More</Link>
+                            <div className="container">
+                                <div className="main-slider__content">
+                                    <div className="main-slider__sub-title-box">
+                                        <p className="main-slider__sub-title">Electrical Company</p>
+                                        <div className="main-slider__sub-title-shape"></div>
+                                    </div>
+                                    <h2 className="main-slider__title">Power Up <br /> Your World</h2>
+                                    <p className="main-slider__text">
+                                        We have been illuminating homes and businesses for over three years, providing <br />
+                                        top-tier electrical solutions and unmatched customer service.
+                                    </p>
+                                    <div className="main-slider__btn-box">
+                                        <Link href="about" className="main-slider__btn thm-btn">Learn More</Link>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-                </SwiperSlide>
-            </Swiper>
-        </section>
-        {/*Main Sllider End */}
+                    </SwiperSlide>
+
+                    {/* Pagination Dots */}
+                    <div className="swiper-pagination"></div>
+                </Swiper>
+            </section>
+            {/* Main Slider End */}
         </>
     )
 }
