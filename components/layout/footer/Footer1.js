60e0190592c9323f517a0b7666d4a2e8066e04b8
samuelcromwell <cromwellsamuel3@gmail.com>
2025-05-21 08:38:40 +0300

    Remove blogs from footer

--- components/layout/footer/Footer1.js
+++ components/layout/footer/Footer1.js
@@ -38,7 +38,7 @@ export default function Footer1() {
                                         <li><Link href="/">Home</Link></li>
                                         <li><Link href="about">About Us</Link></li>
                                         <li><Link href="services">Services</Link></li>
-                                        <li><Link href="blog">Blog</Link></li>
+                                        {/* <li><Link href="blog">Blog</Link></li> */}
                                         <li><Link href="contact">Contact</Link></li>
                                     </ul>
                                 </div>
